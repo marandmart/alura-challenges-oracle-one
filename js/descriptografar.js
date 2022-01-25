d5dea86f3df85bd5001411abcaff9a4b5ed4f04c
Mário André Martins <mario@MacBook-Air-de-Mario.local>
2022-01-25 14:27:49 -0300

    adicionado aviso das letras minusculas e maisuculas. Adicionados estilos para div de aviso. Adicionado footer. Consertado pequeno bug no arquivo de descriptografar

--- js/descriptografar.js
+++ js/descriptografar.js
@@ -1,7 +1,7 @@
 const descriptografar = (mensagem) => {
   let chave = ["ai", "enter", "imes", "ober", "ufat"];
 
-  let regex = new RegExp(chave.join("|"), "ig");
+  let regex = new RegExp(chave.join("|"), "g");
 
   return mensagem.replace(regex, (match) => {
     return match[0];
