e59595cc98bd41d5f12f7aa5e362ae94e9e143d5
marandmart <52830110+marandmart@users.noreply.github.com>
2022-01-25 20:07:04 -0300

    deletado versões antigos dos arquivos

--- Challenge Semana 1/js/descriptografar.js
+++
@@ -1,9 +0,0 @@
-const descriptografar = (mensagem) => {
-  let chave = ["ai", "enter", "imes", "ober", "ufat"];
-
-  let regex = new RegExp(chave.join("|"), "ig");
-
-  return mensagem.replace(regex, (match) => {
-    return match[0];
-  });
-};
