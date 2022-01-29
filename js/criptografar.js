393233daf02db002b8b3030e35cf194be394befd
Mário André Martins <mario@MacBook-Air-de-Mario.local>
2022-01-29 11:21:19 -0300

    alterado o funcionamento da função de criptografia para facilitar o uso

--- js/criptografar.js
+++ js/criptografar.js
@@ -7,9 +7,34 @@ const criptografar = (mensagem) => {
     u: "ufat",
   };
 
-  let regex = new RegExp(Object.keys(chave).join("|"), "g");
 
-  return mensagem.replace(regex, (match) => {
+  let fromKey = new RegExp(Object.keys(chave).join("|"), "g");
+
+  let numbersRegex = new RegExp("[0-9]", "g");
+
+  // retorna os valores de acordo com as chaves
+  let mensagemProcessada = mensagem.replace(fromKey, (match) => {
     return chave[match];
   });
+
+  // retira os números
+  let mensagemSemNumeros = mensagemProcessada.replace(numbersRegex, (match) => {
+    console.log(match);
+    return "";
+  })
+
+  // retira todas as letras maiusculas
+  return mensagemSemNumeros.toLowerCase();
+
 };
+
+
+let teste = "UmA StrInG";
+let teste2 = "UMA STR1NG";
+let teste3 = "DT1F";
+let teste4 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let teste5 = "0123456789";
+let teste6 = "Árètado";
+
+console.log(criptografar(teste4).split("undefined"));
+// console.log(criptografar(teste4));
